dc31b02fd0981fdebeafbc25d706813a996dbab9
hamidnawaz28 <hamid.nawaz@wearenova.co.uk>
2023-06-22 20:35:34 +0500

    removed extras

--- src/popup/screens/index.tsx
+++ src/popup/screens/index.tsx
@@ -1,6 +1,4 @@
 export { default as Scrapper } from './Scrapper'
-export { default as Auth } from './Auth'
 export { default as ExportPage } from './ExportPage'
-export { default as SignIn } from './SignIn'
-export { default as SignUp } from './SignUp'
+
 
