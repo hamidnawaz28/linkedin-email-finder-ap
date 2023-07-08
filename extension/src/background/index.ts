782ae175b1523e4b7f121912d5cc87d3acbdf69c
hamidnawaz29 <hamid.nawaz@trac.co>
2023-07-08 19:11:49 +0500

    Removed extra listners

--- extension/src/background/index.ts
+++ extension/src/background/index.ts
@@ -19,14 +19,10 @@ Browser.runtime.onMessage.addListener(async (request, tabInfo) => {
   const tabId = tab?.id || 0
 
   const responseObj = {
-    [MESSAGING.INITIALIZE_DATA]: async () => await initializeDataBase(),
     [MESSAGING.CHANGE_STATUS]: async () => await changeStatus(data.status),
     [MESSAGING.ADD_PROFILES]: async () => await addProfiles(data.profiles),
     [MESSAGING.UPDATE_A_PROFILE]: async () => await updateAProfile(data.profileUrl, data.profile),
-    [MESSAGING.GET_ALL_PROFILES]: async () => await getAllProfiles(),
     [MESSAGING.GET_ALL_DATA]: async () => await getAllData(),
-    [MESSAGING.UPDATE_PAGES_TO_SCRAP]: async () => await updatePageToScrap(data.pagesToScrap),
-    [MESSAGING.GET_PAGES_TO_SCRAP]: async () => await getPagesToScrap(),
     [MESSAGING.START_COMPLETE_PROFILES_DATA_COLLECTION]: async () =>
       await startCompleteDataCollection(tabId),
   }
